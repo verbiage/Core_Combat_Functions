9ede50f5d9339997f7d3dd072dd4c88e80a0e101
abbottgb <abbottgb@yahoo.com>
2014-10-05 14:45:37 -0500

    Projectiles(Johnathon)
    Player shooting, Bullet physics

---
+++ Projectiles/PlayerBullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBullet : MonoBehaviour {
+	public float timeToDestroy = 2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy (gameObject, timeToDestroy);
+
+	}
+	void OnTriggerEnter(Collider other){
+		//if the player's bullet collides with a tag "enemy" then...
+		if (other.gameObject.tag == "Enemy"){
+			Debug.Log("We have hit the enemy");
+			//Destroy(other.gameObject); destroy our enemy object
+			Destroy(gameObject);//destroys bullet
+}
+	}
+}
---
+++ Projectiles/PlayerShooting.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+	public Rigidbody2D shotPrefab;
+	public float attackSpeed = .5f;
+	public float cooldown;
+	public float shotSpeed = 200f;
+	float yValue = .75f;
+	float xValue = .5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				if (Time.time >= cooldown) {
+						if (Input.GetKey (KeyCode.RightArrow)) {
+								FireRight ();
+						}
+				}
+				if (Time.time >= cooldown) {
+						if (Input.GetKey (KeyCode.LeftArrow)) {
+								FireLeft ();
+						}
+				}
+
+				if (Time.time >= cooldown) {
+						if (Input.GetKey (KeyCode.UpArrow)) {
+								FireUp ();
+						}
+				}
+
+				if (Time.time >= cooldown) {
+						if (Input.GetKey (KeyCode.DownArrow)) {
+								FireDown ();
+						}
+				}
+			}
+
+	void FireRight(){
+		//Rigidbody2D shotPrefabClone = (Rigidbody2D) Instantiate (shotPrefab, transform.position, Quaternion.identity);
+		Rigidbody2D shotPrefabClone = (Rigidbody2D)Instantiate (shotPrefab, new Vector3 (transform.position.x + xValue, transform.position.y, transform.position.z), Quaternion.identity);
+		shotPrefabClone.rigidbody2D.AddForce (Vector3.right * shotSpeed);
+						cooldown = Time.time + attackSpeed;}
+
+	void FireLeft(){
+		//Rigidbody2D shotPrefabClone = (Rigidbody2D) Instantiate (shotPrefab, transform.position, Quaternion.identity);
+		Rigidbody2D shotPrefabClone = (Rigidbody2D)Instantiate (shotPrefab, new Vector3 (transform.position.x - xValue, transform.position.y, transform.position.z), Quaternion.identity);
+		shotPrefabClone.rigidbody2D.AddForce (Vector3.left * shotSpeed);
+						cooldown = Time.time + attackSpeed;}
+
+	void FireUp(){
+		//Rigidbody2D shotPrefabClone = (Rigidbody2D)Instantiate (shotPrefab, transform.position, Quaternion.identity);
+		Rigidbody2D shotPrefabClone = (Rigidbody2D)Instantiate (shotPrefab, new Vector3 (transform.position.x, transform.position.y + yValue, transform.position.z), Quaternion.identity);
+		shotPrefabClone.rigidbody2D.AddForce (Vector3.up * shotSpeed);
+						cooldown = Time.time + attackSpeed;}
+
+	void FireDown(){
+		//Rigidbody2D shotPrefabClone = (Rigidbody2D) Instantiate (shotPrefab, transform.position, Quaternion.identity);
+		Rigidbody2D shotPrefabClone = (Rigidbody2D)Instantiate (shotPrefab, new Vector3 (transform.position.x, transform.position.y - yValue, transform.position.z), Quaternion.identity);
+		shotPrefabClone.rigidbody2D.AddForce (Vector3.down * shotSpeed);
+						cooldown = Time.time + attackSpeed;}
+}
